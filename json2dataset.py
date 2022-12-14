a22d629450de21a410d1c93aa64f299f009f5c47
zhuangBO <p16111121@gs.ncku.edu.tw>
2022-12-14 19:46:56 +0800

    update json2dataset.py

--- json2dataset.py
+++ json2dataset.py
@@ -3,4 +3,4 @@ files = os.listdir('./') #返回指定文件夾下的所有檔案名稱
 #files.remove('json2dataset.py')
 for i in range(len(files)):
     if files[i].endswith(".json"):
-        os.system('labelme_json_to_dataset'+files[i])
+        os.system('labelme_json_to_dataset.exe '+files[i])
